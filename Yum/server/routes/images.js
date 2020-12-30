1b06021d40fcfa243176a3ec112043047b95c888
Sunny Zuo <me@sunnyzuo.com>
2020-12-29 21:18:33 -0700

    feat: image fetching support

--- Yum/server/routes/images.js
+++ Yum/server/routes/images.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const path = require('path');
+const fs = require('fs');
 
 const multer = require('multer');
 const storage = multer.diskStorage({
@@ -22,6 +23,20 @@ router.post('/upload', function (req, res) {
         }
         res.status(200).send({ success: true, filename: req.file.filename });
     });
+});
+
+router.get('/get', function (req, res) {
+    if (!req.body.filename) {
+        res.status(400).send({ error: 'Missing file name'});
+        return;
+    }
+    // we restrict the filename to alphanumeric characters and a period to prevent directory traversal attacks
+    const imagePath = path.normalize(`${__dirname}/../uploads/${req.body.filename.replace(/[^a-zA-Z0-9\.]/, '')}`);
+    if (fs.existsSync(imagePath)) {
+        res.sendFile(imagePath);
+    } else {
+        res.status(404).send({ error: 'Image could not be found'});
+    }
 });
 
 module.exports = router;
