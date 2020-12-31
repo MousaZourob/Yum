a37030c6ec9474d7f449f053687c2d217d6075d6
Abdur Javaid <marjavai@uwaterloo.ca>
2020-12-31 16:30:04 -0500

    fixed merge errors

--- Yum/src/App.js
+++ Yum/src/App.js
@@ -1,25 +1,25 @@
 import logo from "./logo.svg";
 import "./App.css";
-import {BrowserRouter as Router, Route} from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css"
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import Navbar from "./components/navbar.component";
-import Register from "./components/register.component"
+import Register from "./components/register.component";
+import Listings from "./components/listings.js";
 
 function App() {
-
   const createListing = (data, restrictions) => {
     //todo
-  }
+  };
 
   return (
     <Router>
       <Navbar />
-      <Listings createListing={this.createListing} />
-      <br/>
-      <Route path='/users/add' exact component={Register} />
+      <Listings createListing={createListing} />
+      <br />
+      <Route path="/users/add" exact component={Register} />
     </Router>
-    );
+  );
 }
 
 export default App;
