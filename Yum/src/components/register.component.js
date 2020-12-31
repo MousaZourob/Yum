21f00bf17df8ac48f6dabcadb80167d6017d0345
flin42 <feiyanglin2010@gmail.com>
2020-12-30 18:06:06 -0700

    Added Navbar, Registration

---
+++ Yum/src/components/register.component.js
@@ -0,0 +1,104 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+export default class Register extends Component {
+    constructor(props) {
+        super(props);
+
+        this.onChangeName = this.onChangeName.bind(this);
+        this.onChangeEmail = this.onChangeEmail.bind(this);
+        this.onChangePassword = this.onChangePassword.bind(this);
+
+        this.onSubmit = this.onSubmit.bind(this);
+
+        this.state = {
+            name: '',
+            email: '',
+            password: ''
+        }
+    }
+
+    onChangeName(e) {
+        this.setState({
+            name: e.target.value
+        })
+    }
+
+    onChangeEmail(e) {
+        this.setState({
+            email: e.target.value
+        })
+    }
+
+    onChangePassword(e) {
+        this.setState({
+            password: e.target.value
+        })
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+
+        const user = {
+            name: this.state.name,
+            password: this.state.password,
+            email: this.state.email
+
+        }
+        console.log("User " + user + " added");
+
+        axios.post('http://localhost:3000/users/add', user)
+            .then(res => console.log(res.data));
+
+        this.setState({
+            name: '',
+            email: '',
+            password: ''
+        })
+    }
+
+    render() {
+        return (
+            <div className="container" >
+                <h3>Register</h3>
+                <form onSubmit={this.onSubmit}>
+                    <div className="form-group">
+                        <label>Name: </label>
+                        <input type="text"
+                            placeholder="Your Name"
+                            required
+                            className="form-control"
+                            value={this.state.name}
+                            onChange={this.onChangeName}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label>Email: </label>
+                        <input type="text"
+                            placeholder="Email"
+                            required
+                            className="form-control"
+                            value={this.state.email}
+                            onChange={this.onChangeEmail}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label>Password: </label>
+                        <input type="text"
+                            placeholder="Password"
+                            required
+                            className="form-control"
+                            value={this.state.password}
+                            onChange={this.onChangePassword}
+                        />
+                    </div>
+
+                    <div className="form-group">
+                        <input type="submit" value="Register" className="btn btn-primary" />
+                    </div>
+                </form>
+            </div>
+        )
+    }
+
+}
