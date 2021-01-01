15412dc8d2d3cfb83d6b64d11d69c8ba1c90f83e
Abdur <abdur.javaid@gmail.com>
2021-01-01 15:03:01 -0500

    temp commit

--- Yum/src/App.js
+++ Yum/src/App.js
@@ -2,12 +2,24 @@ import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 import Navbar from "./components/navbar.component";
 import Register from "./components/register.component";
 import Listings from "./components/listings.js";
 
 function App() {
+  const { listings, setListings } = useState([]);
+
+  useEffect(() => {
+    let temp = [];
+    axios.get("http://localhost:8000/listings/get").then((response) => {
+      temp = response.data;
+    });
+    setListings(temp);
+  });
+
   return (
     <Router>
       <Navbar />
