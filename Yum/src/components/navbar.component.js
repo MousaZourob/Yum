874ce9a62413593a41e49ee940e9b76c7f069bb2
flin42 <feiyanglin2010@gmail.com>
2021-01-01 14:43:31 -0700

    Added/Updated Create Listing Button

--- Yum/src/components/navbar.component.js
+++ Yum/src/components/navbar.component.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import 'reactjs-popup/dist/index.css';
+//import Listings from "./components/listings.js";
+import Popup from 'reactjs-popup';
+import Listings from './listings';
+
 
 export default class Navbar extends Component {
   render() {
     return (
-      <nav className="navbar navbar-dark bg-primary navbar-expand-lg .text-white font-weight-bold font">
+      <nav className="navbar navbar-dark bg-primary navbar-expand-lg font-weight-bold">
         <Link to="/" className="navbar-brand">YUM</Link>
         <div className="collpase navbar-collapse">
           <ul className="navbar-nav mr-auto">
             <li className="navbar-item">
-              <Link to="/" className="nav-link">View Listings</Link> 
+              <Link to="/" className="nav-link">View Listings</Link>
             </li>
             <li className="navbar-item">
               <Link to="/listings/add" className="nav-link">Create Listing</Link>
@@ -21,6 +26,14 @@ export default class Navbar extends Component {
             </li>
             <li className="navbar-item">
               <Link to="/users/login" className="nav-link ml-auto">Login</Link>
+            </li>
+            <li className="navbar-item">
+              <Popup
+                trigger={<button className="btn btn-primary font-weight-bold"> Create Listing</button>}
+                modal
+                position="center">
+                <div>test</div>
+              </Popup>
             </li>
           </ul>
         </div>
