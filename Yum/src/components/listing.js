b0614b238d3adcfb38696b1aa6a2ae1b7e07d8d4
Abdur Javaid <marjavai@uwaterloo.ca>
2021-01-01 15:51:43 -0500

    Refactored and Pulled Listings from DB

--- Yum/src/components/listing.js
+++ Yum/src/components/listing.js
@@ -2,12 +2,19 @@ import React from "react";
 
 const Listing = (props) => {
   const data = props.data;
+  const restrictions = JSON.parse(data.restrictions);
   return (
     <div>
       <h1>{data.title}</h1>
       <p>{data.description}</p>
       <ul>
-        <li>{data.di_re}</li>
+        {restrictions.map((restriction) => {
+          return (
+            <li>
+              <p>{restriction.label}</p>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
