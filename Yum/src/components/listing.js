7acdb846bba1b376dc73c0c9de9dc2389c581cab
Abdur Javaid <marjavai@uwaterloo.ca>
2021-01-05 17:11:31 -0500

    merged with listings

--- Yum/src/components/listing.js
+++ Yum/src/components/listing.js
@@ -20,59 +20,107 @@ const Listing = (props) => {
   }, []);
 
   let name = data.name;
-  
+
   return (
-    <Popup className="listing"
-    trigger={<div style={{ cursor: "pointer", margin: 24, boxShadow: "5px 5px 5px #d9d9d9" }}>
-      {/*Basic Listing Info*/}
-      <div class="card w-400">
-        <div class="card-body">
-          <h1 >{data.title}</h1>
-          <p>Lister: {name}</p>
+    <Popup
+      className="listing"
+      trigger={
+        <div
+          style={{
+            cursor: "pointer",
+            margin: 24,
+            boxShadow: "5px 5px 5px #d9d9d9",
+          }}
+        >
+          {/*Basic Listing Info*/}
+          <div class="card w-400">
+            <div class="card-body">
+              <h1>{data.title}</h1>
+              <p>Lister: {name}</p>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>}
-
-    modal
-    position="center">
-
-    {/*Specific Listing Info*/}
-    <div class="container" style={{
-      padding: 10,
-      width: "100%"
-    }}
+      }
+      modal
+      position="center"
     >
-      <div class="row" style={{padding: 5, borderRadius: "10px"}}>
-        <div style={{ background: "#cccccc", width: "100%", borderRadius: "10px", padding: 5}} ><h1>{data.title}</h1></div>
-        
-      </div>
-
-      <div class="row" style={{ borderRadius: "10px", marginTop: ".42%" }}>
-        {/*PIC*/}
-        <div class="col-md-6">
-          <img style={{ width: "100%", height: "100%", borderRadius: "10px" }} src={`http://localhost:8000/images/get/${data.image}`} alt="Italian Trulli"></img>  
+      {/*Specific Listing Info*/}
+      <div
+        class="container"
+        style={{
+          padding: 10,
+          width: "100%",
+        }}
+      >
+        <div class="row" style={{ padding: 5, borderRadius: "10px" }}>
+          <div
+            style={{
+              background: "#cccccc",
+              width: "100%",
+              borderRadius: "10px",
+              padding: 5,
+            }}
+          >
+            <h1>{data.title}</h1>
+          </div>
         </div>
-        
-        <div class="col-6" style = {{paddingRight: 24}}>
-          {/*DESCRIPTION*/}
-          <div class="row" style={{ padding: 10, background: "#cccccc", borderRadius: "10px", marginTop: "2%", paddingBottom: 0 }} ><p>{data.description}</p></div>
 
-          {/*RESTRICTIONS*/}
-          <div class="row" style={{ padding: 10, background: "#cccccc", borderRadius: "10px", marginTop: "2.4%"}}><ul
-            style={{ columnCount: 3, wordWrap: "break-word", columnGap: 50, paddingTop: 10}}>
-            {restrictions.map((restriction) => {
-              return (
-                <li>
-                  <p>{restriction.label}</p>
-                </li>
-              );
-            })}
-          </ul>
+        <div class="row" style={{ borderRadius: "10px", marginTop: ".42%" }}>
+          {/*PIC*/}
+          <div class="col-md-6">
+            <img
+              style={{ width: "100%", height: "100%", borderRadius: "10px" }}
+              src={`http://localhost:8000/images/get/${data.image}`}
+              alt="Italian Trulli"
+            ></img>
+          </div>
+
+          <div class="col-6" style={{ paddingRight: 24 }}>
+            {/*DESCRIPTION*/}
+            <div
+              class="row"
+              style={{
+                padding: 10,
+                background: "#cccccc",
+                borderRadius: "10px",
+                marginTop: "2%",
+                paddingBottom: 0,
+              }}
+            >
+              <p>{data.description}</p>
+            </div>
+
+            {/*RESTRICTIONS*/}
+            <div
+              class="row"
+              style={{
+                padding: 10,
+                background: "#cccccc",
+                borderRadius: "10px",
+                marginTop: "2.4%",
+              }}
+            >
+              <ul
+                style={{
+                  columnCount: 3,
+                  wordWrap: "break-word",
+                  columnGap: 50,
+                  paddingTop: 10,
+                }}
+              >
+                {restrictions.map((restriction) => {
+                  return (
+                    <li>
+                      <p>{restriction.label}</p>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </Popup >
-  )
-
+    </Popup>
+  );
+};
 export default Listing;
