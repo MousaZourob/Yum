ba9dc60fc2178b3ee41ac35ea67917c2ea1a4bc4
Abdur Javaid <abdur.javaid@gmail.com>
2021-01-08 22:21:04 -0500

    .

--- Yum/server/routes/chat.js
+++ Yum/server/routes/chat.js
@@ -4,58 +4,44 @@ const ChatRoom = require("../models/chatRoom.model");
 const ChatMessage = require("../models/chatMessage.model");
 const User = require("../models/user.model");
 
-router.get(
-  "/convos",
-  jwt({ secret: process.env.JWT_SECRET, algorithms: ["HS256"] }),
-  async (req, res) => {
+router.get('/convos', jwt({ secret: process.env.JWT_SECRET, algorithms: ["HS256"] }), async (req, res) => {
     const conversations = await ChatRoom.find({ users: req.user._id });
 
     const response = [];
     for (const conversation of conversations) {
-      for (const user of conversation.users) {
-        if (user !== req.user._id) {
-          const userData = await User.findById(user);
-          const lastMessage = await ChatMessage.find({
-            room: conversation._id.toString(),
-          })
-            .sort({ _id: -1 })
-            .limit(1);
-          response.push({
-            roomID: conversation._id.toString(),
-            toName: userData.name,
-            lastMessage: lastMessage[0] ? lastMessage[0].message : "",
-          });
+        for (const user of conversation.users) {
+            if (user !== req.user._id) {
+                const userData = await User.findById(user);
+                const lastMessage = await ChatMessage.find({ room: conversation._id.toString() }).sort({ _id: -1 }).limit(1);
+                response.push({
+                    roomID: conversation._id.toString(),
+                    toName: userData.name,
+                    lastMessage: (lastMessage[0]) ? lastMessage[0].message : ''
+                });
+            }
         }
-      }
     }
 
     res.status(200).send(response);
-  }
-);
+});
 
-router.post(
-  "/newconvo",
-  jwt({ secret: process.env.JWT_SECRET, algorithms: ["HS256"] }),
-  async (req, res) => {
+router.post('/newconvo', jwt({ secret: process.env.JWT_SECRET, algorithms: ["HS256"] }), async (req, res) => {
     if (!req.body.contactID) {
-      return res.status(422).send({ error: "Not all fields were complete" });
+        return res.status(422).send({ error: "Not all fields were complete" });
     }
-    const existingConvo = await ChatRoom.findOne({
-      users: { $all: [req.body.contactID, req.user._id] },
-    });
+    const existingConvo = await ChatRoom.findOne({ users: { $all: [req.body.contactID, req.user._id]}});
     console.log(existingConvo);
     if (existingConvo) {
-      res.status(200).send({ roomID: existingConvo._id.toString() });
-      return;
+        res.status(200).send({ roomID: existingConvo._id.toString() });
+        return;
     }
 
     const newRoomDoc = new ChatRoom({
-      users: [req.body.contactID, req.user._id],
+        users: [req.body.contactID, req.user._id]
     });
 
     const newRoom = await newRoomDoc.save();
     res.status(200).send({ roomID: newRoom._id.toString() });
-  }
-);
+});
 
 module.exports = router;
