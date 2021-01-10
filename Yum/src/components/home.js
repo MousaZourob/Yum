163e335dd575f5868c6ad39af684d22438b2f9f6
f34lin <f34lin@uwaterloo.ca>
2021-01-10 10:31:20 -0500

    feat: updated home page description

--- Yum/src/components/home.js
+++ Yum/src/components/home.js
@@ -22,8 +22,8 @@ function Home() {
 
     return (
         <div>
-            <h1 style={{ textAlign: "center", fontFamily: "Helvetica, sans-serif", margin:"2%" }}>Recent Listings</h1>
-            <Carousel style={{width:"80%", left: "10%"}}>
+            <h1 style={{ textAlign: "center", margin:"2%" }}>Recent Donations</h1>
+            <Carousel style={{width:"80%", left: "10%", background: "lightgrey"}}>
                 <Carousel.Item>
                     <img
                         className="d-block w-100"
@@ -59,10 +59,10 @@ function Home() {
                 </Carousel.Item>
             </Carousel>
 
-
-            <div>
-                <p>Description paragraph</p>
-            </div>
+            <h1 style={{marginTop: "2%", textAlign: "center"}}>Our Mission</h1>
+            <h5 style={{fontWeight: "lighter"}}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </h5>
 
         </div>
 
